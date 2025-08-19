884e909ee9bb20e114c0364bd03ef2c2f802e2c6
chip <chip@seekom.com>
2025-08-19 17:22:56 +1200

    adding the correct ajax call and injecting values into the component

--- plugins/QuickStats/vue/src/QuickStats/QuickStats.vue
+++ plugins/QuickStats/vue/src/QuickStats/QuickStats.vue
@@ -12,10 +12,29 @@
     <MatomoDialog v-model="showDialog">
       <div class="ui-confirm">
         <h2>Visitor Stats</h2>
-        <p>
-          The count is greater than 15 right now!
-        </p>
-
+        <ActivityIndicator :loading="loading"/>
+        <div class="card-container" v-show="!loading">
+          <div class="card">
+            <div class="card-content">
+                Visits: {{ visits }}
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-content">
+              Visitors: {{ visitors }}
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-content">
+              Actions: {{ actions }}
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-content">
+              Visits Converted: {{ visitsConverted }}
+            </div>
+          </div>
+        </div>
         <input type="button" value="OK" role="yes"/>
       </div>
     </MatomoDialog>
@@ -32,24 +51,61 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { MatomoDialog } from 'CoreHome';
+import { MatomoDialog, AjaxHelper, ActivityIndicator } from 'CoreHome';
 
 interface QuickStatsState {
   showDialog: boolean;
+  loading: boolean;
+  visits: string;
+  actions: string;
+  visitors: string;
+  visitsConverted: string;
 }
 
 export default defineComponent({
   components: {
     MatomoDialog,
+    ActivityIndicator,
   },
   data(): QuickStatsState {
     return {
       showDialog: false,
+      loading: true,
+      visits: '1',
+      actions: '1',
+      visitors: '1',
+      visitsConverted: '1',
     };
+  },
+  watch: {
+    showDialog(val) {
+      if (val) {
+        console.log('Dialog opened');
+        this.getQuickStats();
+      } else {
+        console.log('Dialog closed');
+      }
+    },
   },
   methods: {
     showQuickStatsDialog() {
       this.showDialog = true;
+    },
+    getQuickStats() {
+      AjaxHelper.fetch({
+        method: 'Live.getCounters',
+        idSite: 1,
+        lastMinutes: 240,
+        format: 'json',
+      }).then((response) => {
+        console.log('i got responses', response[0]);
+        const resp = response[0];
+        this.visits = resp.visits;
+        this.actions = resp.actions;
+        this.visitors = resp.visitors;
+        this.visitsConverted = resp.visitsConverted;
+        this.loading = false;
+      });
     },
   },
 });
